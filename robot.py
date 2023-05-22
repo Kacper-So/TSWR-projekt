20a1b5f18b3355b061ed23c8bda61d9223f29812
Kacper-So <kacpersobczak152@gmail.com>
2023-05-22 18:16:28 +0200

    + complete inverse kinematics

--- robot.py
+++ robot.py
@@ -7,116 +7,42 @@ from pybullet_utils.bullet_client import BulletClient
 import time
 from math import *
 
-# https://github.com/haruki1526/LeggedRobotsForBullet
-# https://github.com/engineerm-jp/Inverse_Kinematics_YouTube/blob/main/4Legs/kinematics.py#L128
 # https://www.ijstr.org/final-print/sep2017/Inverse-Kinematic-Analysis-Of-A-Quadruped-Robot.pdf
 
-def RotMatrix3D(rotation=[0,0,0],is_radians=True, order='xyz'):
-    
-    roll, pitch, yaw = rotation[0], rotation[1], rotation[2]
-
-    # convert to radians is the input is in degrees
-    if not is_radians: 
-        roll = math.radians(roll)
-        pitch = math.radians(pitch)
-        yaw = math.radians(yaw)
-    
-    # rotation matrix about each axis
-    rotX = np.matrix([[1, 0, 0], [0, math.cos(roll), -math.sin(roll)], [0, math.sin(roll), math.cos(roll)]])
-    rotY = np.matrix([[math.cos(pitch), 0, math.sin(pitch)], [0, 1, 0], [-math.sin(pitch), 0, math.cos(pitch)]])
-    rotZ = np.matrix([[math.cos(yaw), -math.sin(yaw), 0], [math.sin(yaw), math.cos(yaw), 0], [0, 0, 1]])
-    
-    # rotation matrix order (default: pitch -> roll -> yaw)
-    if order == 'xyz': rotationMatrix = rotZ * rotY * rotX
-    elif order == 'xzy': rotationMatrix = rotY * rotZ * rotX
-    elif order == 'yxz': rotationMatrix = rotZ * rotX * rotY
-    elif order == 'yzx': rotationMatrix = rotX * rotZ * rotY
-    elif order == 'zxy': rotationMatrix = rotY * rotX * rotZ
-    elif order == 'zyx': rotationMatrix = rotX * rotY * rotZ
-    
-    return rotationMatrix # roll pitch and yaw rotation 
-
-
-def point_to_rad(p1, p2): # converts 2D cartesian points to polar angles in range 0 - 2pi
-        
-    if (p1 > 0 and p2 >= 0): return math.atan(p2/(p1))
-    elif (p1 == 0 and p2 >= 0): return math.pi/2
-    elif (p1 < 0 and p2 >= 0): return -abs(math.atan(p2/p1)) + math.pi
-    elif (p1 < 0 and p2 < 0): return math.atan(p2/p1) + math.pi
-    elif (p1 > 0 and p2 < 0): return -abs(math.atan(p2/p1)) + 2*math.pi
-    elif (p1 == 0 and p2 < 0): return math.pi * 3/2
-    elif (p1 == 0 and p2 == 0): return math.pi * 3/2 # edge case
-
-
 class leg():
-    def __init__(self, isRigth, origin):
+    def __init__(self, isRigth, origin, phi):
         self.origin = origin
         self.isRight = isRigth
-        self.link_1 = 0
+        self.link_1 = 0.
         self.link_2 = 0.18
         self.link_3 = 0.18
-        self.phi = radians(90)
+        self.phi = phi
 
-    def inverse_kinematics(self, refPosOrient):
-        refPosOrient = np.asarray((inv(RotMatrix3D(refPosOrient[3:], True)) @ ((np.array(refPosOrient[:3]) + self.origin).transpose())).transpose())
-        refPosOrient = np.asarray([refPosOrient[0][0], refPosOrient[1][0], refPosOrient[2][0]] - self.origin).flatten()
-        x, y, z = refPosOrient[0], refPosOrient[1], refPosOrient[2]    # unpack coordinates
-        
-        # length of vector projected on the YZ plane. equiv. to len_A = sqrt(y**2 + z**2)
-        len_A = norm([0,y,z])   
-        
-        # a_1 : angle from the positive y-axis to the end-effector (0 <= a_1 < 2pi)
-        # a_2 : angle bewtween len_A and leg's projection line on YZ plane
-        # a_3 : angle between link1 and length len_A
-        a_1 = point_to_rad(y,z)                     
-        a_2 = asin(sin(self.phi)*self.link_1/len_A) 
-        a_3 = pi - a_2 - self.phi                   
-        
-        # angle of link1 about the x-axis 
-        if self.isRight: theta_1 = a_1 - a_3
-        else: 
-            theta_1 = a_1 + a_3
-            if theta_1 >= 2*pi: theta_1 -= 2*pi
-        
-        j2 = np.array([0,self.link_1*cos(theta_1),self.link_1*sin(theta_1)])
-        j4 = np.array(refPosOrient)
-        j4_2_vec = j4 - j2 # vector from j2 to j4
-        
-        if self.isRight: R = theta_1 - self.phi - pi/2
-        else: R = theta_1 + self.phi - pi/2
-        
-        # create rotation matrix to work on a new 2D plane (XZ_)
-        rot_mtx = RotMatrix3D([-R,0,0],is_radians=True)
-        j4_2_vec_ = rot_mtx * (np.reshape(j4_2_vec,[3,1]))
-        
-        # xyz in the rotated coordinate system + offset due to link_1 removed
-        x_, y_, z_ = j4_2_vec_[0], j4_2_vec_[1], j4_2_vec_[2]
-        
-        len_B = norm([x_, z_]) # norm(j4-j2)
-        
-        # handling mathematically invalid input, i.e., point too far away to reach
-        if len_B >= (self.link_2 + self.link_3): 
-            len_B = (self.link_2 + self.link_3) * 0.99999
-            # self.node.get_logger().warn('target coordinate: [%f %f %f] too far away' % (x, y, z))
-            print('target coordinate: [%f %f %f] too far away' % (x, y, z))
-        
-        # b_1 : angle between +ve x-axis and len_B (0 <= b_1 < 2pi)
-        # b_2 : angle between len_B and link_2
-        # b_3 : angle between link_2 and link_3
-        b_1 = point_to_rad(x_, z_)  
-        b_2 = acos((self.link_2**2 + len_B**2 - self.link_3**2) / (2 * self.link_2 * len_B)) 
-        b_3 = acos((self.link_2**2 + self.link_3**2 - len_B**2) / (2 * self.link_2 * self.link_3))  
-        
-        # assuming theta_2 = 0 when the leg is pointing down (i.e., 270 degrees offset from the +ve x-axis)
-        theta_2 = b_1 - b_2    
-        theta_3 = pi - b_3
-        
-        
-        # modify angles to match robot's configuration (i.e., adding offsets)
-        # angles = self.angle_corrector(angles=[theta_1, theta_2, theta_3], is_right=is_right)
-
-        # print(degrees(angles[0]))
-        return [theta_1, theta_2 ,theta_3]
+    def inverse_kinematics(self, refPosOrient, legPos):
+        position = refPosOrient[:3]
+        roll = refPosOrient[3]
+        yaw = refPosOrient[4]
+        pitch = refPosOrient[5]
+        R_x = np.array([[1., 0., 0., 0.], [0., math.cos(roll), -math.sin(roll), 0.], [0., math.sin(roll), math.cos(roll), 0.], [0., 0., 0., 1.]])
+        R_y = np.array([[math.cos(yaw), 0., math.sin(yaw), 0.], [0., 1., 0., 0.], [-math.sin(yaw), 0., math.cos(yaw), 0.], [0., 0., 0., 1.]])
+        R_z = np.array([[math.cos(pitch), -math.sin(pitch), 0., 0.], [math.sin(pitch), math.cos(pitch), 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
+        R_xyz = R_x @ R_y @ R_z
+        T_M = R_xyz @ np.array([[1, 0, 0, position[0]], [0, 1, 0, position[1]], [0, 0, 1, position[2]], [0, 0, 0, 1]])
+        T_leg = T_M @ np.array([[math.cos(self.phi), 0, math.sin(self.phi), self.origin[0]], [0, 1, 0, 0], [-math.sin(self.phi), 0, math.cos(self.phi), self.origin[1]], [0, 0, 0, 1]])
+        x_leg_0 = T_leg[0][3]
+        y_leg_0 = T_leg[1][3]
+        z_leg_0 = T_leg[2][3]
+        x_leg = x_leg_0 + legPos[0] - self.origin[0]
+        y_leg = y_leg_0 + legPos[1] - self.origin[2]
+        z_leg = z_leg_0 + legPos[2] - self.origin[1]
+        theta_1 = -math.atan2(-y_leg, x_leg) - math.atan2(math.sqrt(math.pow(x_leg, 2) + math.pow(y_leg, 2) - math.pow(self.link_1,2)), -self.link_1)
+        D = (math.pow(x_leg, 2) + math.pow(y_leg, 2) - math.pow(self.link_1, 2) + math.pow(z_leg, 2) - math.pow(self.link_2, 2) - math.pow(self.link_3, 2)) / (2 * self.link_2 * self.link_3)
+        if self.isRight:
+            theta_3 = math.atan2(-math.sqrt(1 - math.pow(D, 2)), D)
+        else:
+            theta_3 = math.atan2(math.sqrt(1 - math.pow(D, 2)), D)
+        theta_2 = math.atan2(z_leg, math.sqrt(math.pow(x_leg, 2) + math.pow(y_leg, 2) - math.pow(self.link_1,2))) - math.atan2(self.link_3 * math.sin(theta_3), self.link_2 + self.link_3 * math.cos(theta_3))
+        return [theta_1, theta_2, theta_3]
 
 class Robot:
     def __init__(self, timeStep, startPosition, startOrientation,):
@@ -127,29 +53,27 @@ class Robot:
         self.client.setTimeStep(timeStep)
         for j in range(self.client.getNumJoints(0)):
             self.client.setJointMotorControl2(0, j, pb.POSITION_CONTROL, force=0)
-        self.RF = leg(True, np.array([0.2,-0.11,0.]))
-        self.LF = leg(False, np.array([0.2,0.11,0.]))
-        self.RH = leg(True, np.array([-0.2,-0.11,0.]))
-        self.LH = leg(False, np.array([-0.2,0.11,0.]))
+        self.RF = leg(True, np.array([0.2,-0.11,0.]), -math.pi/2)
+        self.LF = leg(True, np.array([0.2,0.11,0.]), math.pi/2)
+        self.RH = leg(False, np.array([-0.2,-0.11,0.]), math.pi/2)
+        self.LH = leg(False, np.array([-0.2,0.11,0.]), -math.pi/2)
         
     def set_control(self, u):
         v = [0,0,0,0,0,0,0,0,0,0,0,0]
         for i in range(self.client.getNumJoints(0)):
-            self.client.setJointMotorControl2(0, i, pb.POSITION_CONTROL, targetPosition=u[i], targetVelocity=v[i], force=1)
+            self.client.setJointMotorControl2(0, i, pb.POSITION_CONTROL, targetPosition=u[i], targetVelocity=v[i], force=10)
 
     def calculate_inverse_kinematics(self):
-        robot_cords = [0,0,-0.1,0,0,0]
-        uRF = self.RF.inverse_kinematics(robot_cords)
-        uLF = self.LF.inverse_kinematics(robot_cords)
-        uRH = self.RH.inverse_kinematics(robot_cords)
-        uLH = self.LH.inverse_kinematics(robot_cords)
+        robot_cords = [0, 0.25, 0, 0 * math.pi/180, 0 * math.pi/180, 0 * math.pi/180]
+        legPos = [0, 0, 0]
+        uRF = self.RF.inverse_kinematics(robot_cords, legPos)
+        uLF = self.LF.inverse_kinematics(robot_cords, legPos)
+        uRH = self.RH.inverse_kinematics(robot_cords, legPos)
+        uLH = self.LH.inverse_kinematics(robot_cords, legPos)
         u = [uRF[0], uRF[1], uRF[2], uLF[0], uLF[1], uLF[2], uRH[0], uRH[1], uRH[2], uLH[0], uLH[1], uLH[2]]
-        # print(u)
-        # u = [0,0,0,0,0,0,0,0,0,0,0,0]
         return u
 
 
     def simulation_step(self):
         robotPosition, _ = self.client.getBasePositionAndOrientation(self.robotId)
-        pb.resetDebugVisualizerCamera(cameraDistance=1.0, cameraYaw=180, cameraPitch=-10, cameraTargetPosition=robotPosition)
         self.client.stepSimulation()
